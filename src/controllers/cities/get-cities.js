158b0c092773ad4a6582036b2b16e7cb9f751278
usmanfaisal60 <usman.faisal60@gmail.com>
2022-03-09 14:19:02 +0500

    - promises explained

--- src/controllers/cities/get-cities.js
+++ src/controllers/cities/get-cities.js
@@ -1,7 +1,16 @@
+const axios = require("axios");
 const { CitiesService } = require('../../services');
 
 module.exports = (req, res) => {
-    const cities = CitiesService.getCities();
-    res.send(cities);
-    res.end();
+    axios.get("https://raw.githubusercontent.com/datasets/world-cities/master/data/world-cities.csv")
+        .then(axiosResponse => {
+            res.set("Content-Type", "text/plain")
+            res.send(axiosResponse.data);
+            res.end();
+        })
+        .catch(err => console.error(err));
+
+    // const cities = CitiesService.getCities();
+    // res.send(cities);
+    // res.end();
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const routes = require("./controllers");
-
 const app = express();
 
-app.use(routes());
+const startNodejsServer = () => {
+    app.use(routes());
+    app.listen(3000, () => console.log("Server started on 3000 port"));
+}
 
-app.listen(3000, () => console.log("Server started on 3000 port"));
+startNodejsServer();
